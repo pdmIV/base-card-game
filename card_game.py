421ecd2a60b0303523a6121684d2fffbd923e3eb
Peter Mora IV <129004174+pdmIV@users.noreply.github.com>
2024-05-28 20:38:48 -0500

    Add files via upload

---
+++ card_game.py
@@ -0,0 +1,88 @@
+import random
+from collections import deque
+colors = ['clubs', 'spades', 'gold coins', 'cups']
+values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+
+
+class Card:
+    def __init__(self, color, value):
+        self.value = value
+        self.color = color
+
+    def as_str(self):
+        return self.value + " of " + self.color
+
+
+class Deck:
+    def __init__(self, shuffled=False):
+        global colors
+        global values
+
+        self.cards = deque()
+
+        for color in colors:
+            for value in values:
+                self.cards.append(Card(color, value))
+        
+        if shuffled:
+            self.shuffle()
+    
+    def shuffle(self):
+        temp_list = list(self.cards)
+        random.shuffle(temp_list)
+        self.cards = deque(temp_list)
+
+class Player:
+    def __init__(self, name: str):
+        self.name = name
+        self.cards = set()
+        self.str_cards = ""
+        self.num_cards = 0
+
+    def recieve_card(self, card: Card):
+        self.cards.add(card)
+        if self.num_cards == 0:
+            self.str_cards += card.as_str()
+        else:
+            addition = ", " + card.as_str()
+            self.str_cards += addition
+    
+    
+    def reveal(self):
+        return self.str_cards
+
+
+class Game:
+    def __init__(self):
+        self.deck = Deck()
+        self.players = list()
+
+    def add_player(self, name: str):
+        self.players.append(Player(name))
+
+    def deal(self, amount=1):
+        amt = amount
+        while amt > 0:
+            for p in self.players:
+                p.recieve_card(self.deck.cards.popleft())
+            amt -= 1
+
+    def shuffle_deck(self):
+        self.deck.shuffle()
+    
+    def reveal_state(self):
+        for player in self.players:
+            print(player.name + " posesses " + player.reveal())
+
+# Run Program - example 3 player game with a shuffled deck
+game = Game()
+
+game.add_player("Peter")
+game.add_player("Sofia")
+game.add_player("Francesco")
+
+game.shuffle_deck()
+
+game.deal()
+game.reveal_state()
+            
